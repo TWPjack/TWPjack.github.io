eaa83f4bf8df82a24041ea8364e60fb24614d21d
jackc8763 <jackfirstfound@gmail.com>
2021-03-10 15:10:59 +0800

    dedsf

--- js/script.js
+++ js/script.js
@@ -10,14 +10,14 @@ var myFullpage = new fullpage('#fullpage', {
   afterLoad: function(origin, destination, direction){
     //section 2
     if(destination.index == 1){
-        document.querySelector('#section2').querySelector('.ttt').style.left = 0 + 'px';
-        document.querySelector('#section2').querySelector('p').style.opacity = 1;
+        // document.querySelector('#section2').querySelector('.ttt').style.left = 0 + 'px';
+        // document.querySelector('#section2').querySelector('p').style.opacity = 1;
     }
 
     //back to original state
     else if(origin && origin.index == 1){
-        document.querySelector('#section2').querySelector('.ttt').style.left = 130 + '%';
-        document.querySelector('#section2').querySelector('p').style.opacity = 0;
+        // document.querySelector('#section2').querySelector('.ttt').style.left = 130 + '%';
+        // document.querySelector('#section2').querySelector('p').style.opacity = 0;
     }
 
 }
